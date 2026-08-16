b66546df65561429b9441ea29f596e26796d40be
CAWNPORETIGERSXI <anujtpclasses@gmail.com>
2026-08-16 13:06:33 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,56 @@
+// ==========================================
+// CRICKPULSE - FIREBASE CONFIGURATION
+// ==========================================
+
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
+
+// ==========================================
+// FIREBASE CONFIG
+// ==========================================
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyB9X_8sg565D3cSQAB3D-6Arr9GjD1GQ6s",
+
+    authDomain: "crickpulse-86857.firebaseapp.com",
+
+    projectId: "crickpulse-86857",
+
+    storageBucket: "crickpulse-86857.firebasestorage.app",
+
+    messagingSenderId: "7752708835",
+
+    appId: "1:7752708835:web:1efdef8ed098b490e3eb5e"
+
+};
+
+
+// ==========================================
+// INITIALIZE FIREBASE
+// ==========================================
+
+const app = initializeApp(firebaseConfig);
+
+
+// ==========================================
+// INITIALIZE AUTH
+// ==========================================
+
+const auth = getAuth(app);
+
+
+// ==========================================
+// EXPORT
+// ==========================================
+
+export {
+    app,
+    auth
+};
